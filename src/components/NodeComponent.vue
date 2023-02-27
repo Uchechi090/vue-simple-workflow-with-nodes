709b2fc1a6efd39329d0e27ffc671916542a2f49
Uchechi090 <37576448+Uchechi090@users.noreply.github.com>
2023-02-27 16:01:30 +0100

    1 file modified

--- src/components/NodeComponent.vue
+++ src/components/NodeComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <VueFlow
+    <!-- <VueFlow
       :width="250"
       :height="600"
       v-model="elements"
@@ -10,11 +10,20 @@
       :default-viewport="{ zoom: 1.5 }"
       :min-zoom="0.4"
       :max-zoom="7"
+    > -->
+    <VueFlow
+      v-model="elements"
+      class="basicflow"
+      :default-edge-options="{ type: 'smoothstep' }"
+      :default-viewport="{ zoom: 1.5 }"
+      :min-zoom="0.2"
+      :max-zoom="4"
+      fit-view-on-init
     >
       <!-- <template #node-toolbar="nodeProps">
         <SwitchNode :data="nodeProps.data" :label="nodeProps.label" />
       </template> -->
-      <Background />
+      <Background :pattern-color="'#aaa'" gap="8" />
     </VueFlow>
   </div>
 </template>
@@ -52,67 +61,130 @@ export default defineComponent({
       width: '40px',
       textAlign: 'center',
     };
-    const { onConnect, nodes, edges, addEdges, addNodes } = useVueFlow({
-      fitViewOnInit: true,
-      nodes: [
-        {
-          id: '1',
-          type: 'toolbar',
-          // label: 'source',
-          data: { toolbarPosition: Position.Left },
-          position: { x: -350, y: 150 },
-          style: sourceStyle,
-        },
-        {
-          id: '2',
-          type: 'toolbar',
-          // label: 'manipulator',
-          // data: { toolbarPosition: Position.Top, toolbarVisible: true },
-          data: { toolbarPosition: Position.Top },
-          position: { x: -180, y: 130 },
-          style: manipulatorStyle,
-        },
-        {
-          id: '3',
-          type: 'toolbar',
-          // label: 'predictor',
-          data: { toolbarPosition: Position.Right },
-          position: { x: -30, y: 110 },
-          style: predictorStyle,
-        },
-      ],
-      edges: [
-        {
-          id: 'e1a-2',
-          source: '1',
-          sourceHandle: 'a',
-          target: '2',
-          markerEnd: MarkerType.ArrowClosed,
-          style: () => ({
-            stroke: '#000000',
-            filter: 'invert(100%)',
-          }),
-        },
-        {
-          id: 'e1b-3',
-          source: '2',
-          sourceHandle: 'b',
-          target: '3',
-          markerEnd: MarkerType.ArrowClosed,
-          style: () => ({
-            stroke: '#000000',
-            filter: 'invert(100%)',
-          }),
-        },
-      ],
+    const { onConnect, addEdges } = useVueFlow({
+      // fitViewOnInit: true,
+      // nodes: [
+      //   {
+      //     id: '1',
+      //     type: 'toolbar',
+      //     // label: 'source',
+      //     data: { toolbarPosition: Position.Left },
+      //     position: { x: -350, y: 150 },
+      //     style: sourceStyle,
+      //   },
+      //   {
+      //     id: '2',
+      //     type: 'toolbar',
+      //     // label: 'manipulator',
+      //     // data: { toolbarPosition: Position.Top, toolbarVisible: true },
+      //     data: { toolbarPosition: Position.Top },
+      //     position: { x: -180, y: 130 },
+      //     style: manipulatorStyle,
+      //   },
+      //   {
+      //     id: '3',
+      //     type: 'toolbar',
+      //     // label: 'predictor',
+      //     data: { toolbarPosition: Position.Right },
+      //     position: { x: -30, y: 110 },
+      //     style: predictorStyle,
+      //   },
+      // ],
+      // edges: [
+      //   {
+      //     id: 'e1a-2',
+      //     source: '1',
+      //     sourceHandle: 'a',
+      //     target: '2',
+      //     markerEnd: MarkerType.ArrowClosed,
+      //     style: () => ({
+      //       stroke: '#000000',
+      //       filter: 'invert(100%)',
+      //     }),
+      //   },
+      //   {
+      //     id: 'e1b-3',
+      //     source: '2',
+      //     sourceHandle: 'b',
+      //     target: '3',
+      //     markerEnd: MarkerType.ArrowClosed,
+      //     style: () => ({
+      //       stroke: '#000000',
+      //       filter: 'invert(100%)',
+      //     }),
+      //   },
+      // ],
     });
+
+    const elements = ref([
+      {
+        id: '1',
+        type: 'input',
+        // type: 'toolbar',
+        // label: 'source',
+        // data: { toolbarPosition: Position.Left },
+        // position: { x: -350, y: 150 },
+        position: { x: 0, y: 50 },
+        sourcePosition: Position.Right,
+        // style: sourceStyle,
+      },
+      {
+        id: '2',
+        // type: 'toolbar',
+        // label: 'manipulator',
+        // data: { toolbarPosition: Position.Top, toolbarVisible: true },
+        // data: { toolbarPosition: Position.Top },
+        // position: { x: -180, y: 130 },
+        position: { x: 250, y: 100 },
+        sourcePosition: Position.Right,
+        targetPosition: Position.Left,
+        // style: manipulatorStyle,
+      },
+      {
+        id: '3',
+        type: 'output',
+        // type: 'toolbar',
+        // label: 'predictor',
+        // data: { toolbarPosition: Position.Right },
+        // position: { x: -30, y: 110 },
+        position: { x: 750, y: 50 },
+        sourcePosition: Position.Right,
+        targetPosition: Position.Left,
+        // style: predictorStyle,
+      },
+      {
+        id: 'e1-2',
+        source: '1',
+        // sourceHandle: 'a',
+        target: '2',
+        markerEnd: MarkerType.ArrowClosed,
+        // style: () => ({
+        //   stroke: '#000000',
+        //   filter: 'invert(100%)',
+        // }),
+      },
+      {
+        id: 'e2-3',
+        source: '2',
+        // sourceHandle: 'b',
+        target: '3',
+        markerEnd: MarkerType.ArrowClosed,
+        // style: () => ({
+        //   stroke: '#000000',
+        //   filter: 'invert(100%)',
+        // }),
+      },
+    ]);
+    // onPaneReady(({ fitView }) => {
+    //   fitView();
+    // });
 
     onConnect((params) => addEdges([params]));
 
-    onMounted(() => {
-      // add nodes to parent
-      addNodes([]);
-    });
+    // onMounted(() => {
+    //   // add nodes to parent
+    //   addNodes([]);
+    // });
 
     const defaultNodeStyle = {
       border: '1px solid #10b981',
@@ -169,10 +241,10 @@ export default defineComponent({
     //   },
     // ]);
     return {
-      // elements,
+      elements,
       onConnect,
-      nodes,
-      edges,
+      // nodes,
+      // edges,
       addEdges,
     };
   },
